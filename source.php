c25fd96239fc870f3c14bd876e9aefe0e3091268
Smiks <sandi.mikus@gmail.com>
2013-01-24 20:31:39 +0100

    optimization

--- source.php
+++ source.php
@@ -1,5 +1,7 @@
 <?php
 $end=100;
+$n1=3;
+$n2=5;
 $array=array();
 $array[0][1]="fizz";
 $array[1][0]="buzz";
@@ -7,8 +9,8 @@ $array[0][0]="fizzbuzz";
 for($i=1;$i<=$end;$i++) 
 {
 	$array[1][1]=$i;
-	$fizz=ceil(($i%3)/$end);
-	$buzz=ceil(($i%5)/$end);
-	echo("<li>".$array[$fizz][$buzz]);
+	$fizz = $i % $n1 && true;
+	$buzz = $i % $n2 && true;
+	echo("<li>".$array[$fizz][$buzz]."</li>");
 }
 ?>
